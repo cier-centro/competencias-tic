6529b95b322d13e33e1a0d34aff87f462dbeb4eb
YesidDaza <u1801475@unimilitar.edu.co>
2018-03-13 12:59:25 -0500

    Changes in structure

--- encuesta/js/index.js
+++ encuesta/js/index.js
@@ -1,3 +1,8 @@
 window.onload = function(){
 
 };
+
+function resizeIframe(obj) {
+    console.log(obj.contentWindow.document.body.scrollHeight);
+    obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
+}
