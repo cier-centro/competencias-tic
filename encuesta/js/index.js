26e0465e30abf6b153de014965fdf541112da3cf
YesidDaza <u1801475@unimilitar.edu.co>
2018-03-14 10:40:19 -0500

    Change size elements

--- encuesta/js/index.js
+++ encuesta/js/index.js
@@ -1,8 +1,3 @@
 window.onload = function(){
 
 };
-
-function resizeIframe(obj) {
-    console.log(obj.contentWindow.document.body.scrollHeight);
-    obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
-}
