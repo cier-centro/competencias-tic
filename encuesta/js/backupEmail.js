78281d4cddee4f19cf889393ff1787cd70030167
Yesid Daza <yiye_91@hotmail.com>
2018-04-13 17:41:08 -0500

    Changes in form url and backup

--- encuesta/js/backupEmail.js
+++ encuesta/js/backupEmail.js
@@ -1,9 +1,12 @@
 //https://docs.google.com/forms/d/e/1FAIpQLSckfMr2zjvGo08e9pPAE1iZ8vQnQc1poZPXRIS6oNIt-an4eA/viewform?usp=pp_url&entry.26647293=Daza&entry.1902537249=3213213&entry.1765249514=ydaza@gitei.edu.co&entry.618703803=Hombre&entry.781656536=21+-+30&entry.805018916=Todas+las+asignaturas&entry.1886189987=B%C3%A1sica+secundaria&entry.1785351721=Aprobado&entry.2055329056=Decreto+1278+de+2002&entry.2057563961=Cmarca&entry.1229277571=Idei&entry.1444558031=1&entry.1026490934=2&entry.269129317=1&entry.1298042726=0&entry.134131095=1&entry.965515678=1&entry.1156841145=0&entry.1364704044=1&entry.1109956124=1&entry.643628948=2&entry.1188526696=2&entry.945005970=1&entry.1205006325=2&entry.975896223=2&entry.1809757995=2&entry.15588726=2&entry.1895296045=2&entry.1180829362=1&entry.1722097493=1&entry.1409288934=2&entry.1932445958=2&entry.2730175=1&entry.1772354376=1&entry.1020106168=2&entry.1323187994=2&entry.750359508=1&entry.1756700184=1&entry.886186224=2&entry.2104735137=1&entry.326826978=2&entry.897728948=1&entry.221005505=2&entry.2014094651=1&entry.57310449=2&entry.132293132=1&entry.1674161771=2&entry.100476736=1&entry.68444739=2&entry.879669428=2&entry.1348118598=1&entry.1893795485=1&entry.1914131614=2&entry.1658491068=2&entry.1362498238=1&entry.291784763=1&entry.389150359=2&entry.1678963136=2&entry.709887415=1&entry.296108911=2&entry.23575497=1&entry.1640455716=2&entry.543150211=2&entry.991620359=2&entry.1011297224=1&entry.97989396=2&entry.2132279693=2&entry.1152401402=2&entry.1333687825=2&entry.2025947001=1&entry.1760324324=2&entry.1457147085=2&entry.1977251599=2&entry.864560536=1&entry.1454866104=2&entry.1831711108=2&entry.779870544=2&entry.1980468116=1&entry.405603422=2&entry.759214817=2&entry.475436336=2&entry.1577749116=1&entry.867166008=2&entry.300015365=2&entry.1412961250=2&entry.1890098527=1&entry.409643532=2&entry.1053947480=2&entry.1048383661=2&entry.1459922221=1&entry.1811410652=1&entry.63849312=2&entry.53343516=2&entry.1968572966=1&entry.757653363=2&entry.852040138=2&entry.464214423=1&entry.95799373=2&entry.2095612640=2&entry.1157334871=1&entry.1288255243=1
 
+//nuevo prellenado
+//https://docs.google.com/forms/d/e/1FAIpQLSeBySBgMcTKjsB1wW_1n-diGiZ0nNvegPuz9XVtbcq7g6JrTg/viewform?usp=pp_url&entry.26647293=YES&entry.1902537249=12345678&entry.1033726661=ydaza@gitei.edu.co&entry.618703803=Hombre&entry.781656536=21+-+30&entry.805018916=Filosof%C3%ADa&entry.493788756=qwerty&entry.1886189987=Preescolar&entry.1472946729=Aprobado&entry.2055329056=Decreto+1278+de+2002&entry.1156732022=S%C3%AD&entry.467908561=S%C3%AD&entry.2057563961=ARAUCA&entry.700026055=dfghj&entry.1229277571=rtyu&entry.155055639=cvbn&entry.1444558031=1&entry.1026490934=2&entry.269129317=1&entry.1298042726=2&entry.134131095=2&entry.965515678=2&entry.1156841145=1&entry.1364704044=2&entry.1109956124=1&entry.643628948=2&entry.1188526696=2&entry.945005970=1&entry.1205006325=2&entry.975896223=2&entry.1809757995=2&entry.15588726=1&entry.1895296045=1&entry.1180829362=2&entry.1722097493=2&entry.1409288934=0&entry.1932445958=1&entry.2730175=1&entry.1772354376=2&entry.1020106168=2&entry.1323187994=1&entry.750359508=1&entry.1756700184=1&entry.886186224=2&entry.2104735137=2&entry.326826978=1&entry.897728948=1&entry.221005505=1&entry.2014094651=2&entry.57310449=1&entry.132293132=2&entry.1674161771=2&entry.100476736=2&entry.68444739=1&entry.879669428=2&entry.1348118598=2&entry.1893795485=2&entry.1914131614=1&entry.1658491068=1&entry.1362498238=1&entry.291784763=2&entry.389150359=2&entry.1678963136=2&entry.709887415=1&entry.296108911=2&entry.23575497=2&entry.1640455716=1&entry.543150211=1&entry.991620359=2&entry.1011297224=2&entry.97989396=1&entry.2132279693=2&entry.1152401402=2&entry.1333687825=1&entry.2025947001=2&entry.1760324324=2&entry.1457147085=2&entry.1977251599=1&entry.864560536=1&entry.1454866104=2&entry.1831711108=2&entry.779870544=2&entry.1980468116=1&entry.405603422=1&entry.759214817=2&entry.475436336=2&entry.1577749116=1&entry.867166008=1&entry.300015365=2&entry.1412961250=2&entry.1890098527=1&entry.409643532=1&entry.1053947480=1&entry.1048383661=2&entry.1459922221=2&entry.1811410652=1&entry.63849312=2&entry.53343516=2&entry.1968572966=2&entry.757653363=2&entry.852040138=1&entry.464214423=1&entry.95799373=1&entry.2095612640=1&entry.1157334871=1&entry.1288255243=2
+
 function onFormSubmit(e) {
   var sheet = SpreadsheetApp.getActiveSheet();
   var email = e.namedValues['Correo Electrónico'][0];
-  var name = e.namedValues['Nombre'][0];
+  var name = e.namedValues['Nombres y Apellidos'][0];
   var message='funcioneeeee '
 
   var categoriesIndices = {
@@ -89,15 +92,17 @@ function onFormSubmit(e) {
 
   //https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf
   //MailApp.sendEmail(email, "fuuuu", message,{htmlbody:htmlEmail});
+
   mailBody = "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\">";
-  mailBody += 'Hola \nEl resultado de tu prueba <br><br>';
-  mailBody += '<img src='+ graphUrl +'><br><br>';
+  mailBody += 'Hola ' + name + ', el resultado de tu prueba se muestra a continuación:<br><br>';
   mailBody += table + '<br><br>';
+  mailBody += '<img src='+ graphUrl +'><br><br>';
   mailBody += 'También estan disponibles <a href=' + url + '>AQUI</a><br><br>';
   mailBody += '<a href="'+ giteiUrl + '"><img style="width: 49%; max-width: 264px;" src=' + giteiLogoUrl + '></a>';
   mailBody += '<img style="width: 0.555%; max-width: 3px;" src=' + separatorLogoUrl + '>';
   mailBody += '<a href="'+ unalUrl + '"><img style="width: 38%; max-width: 204px;" src=' + unalLogoUrl + '></a>';
   MailApp.sendEmail(email, "Evaluación competencias TIC", message,{htmlBody: mailBody});
+
 }
 
  function loadJSFromHTMLFile() {
