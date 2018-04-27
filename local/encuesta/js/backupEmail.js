4c945092f0d6c88956b24e4a03b94ab615ea6539
YesidDaza <yiye_91@hotmail.com>
2018-04-27 10:01:10 -0500

    local folder is created

---
+++ local/encuesta/js/backupEmail.js
@@ -0,0 +1,133 @@
+//antiguo prellenado
+//https://docs.google.com/forms/d/e/1FAIpQLSckfMr2zjvGo08e9pPAE1iZ8vQnQc1poZPXRIS6oNIt-an4eA/viewform?usp=pp_url&entry.26647293=Daza&entry.1902537249=3213213&entry.1765249514=ydaza@gitei.edu.co&entry.618703803=Hombre&entry.781656536=21+-+30&entry.805018916=Todas+las+asignaturas&entry.1886189987=B%C3%A1sica+secundaria&entry.1785351721=Aprobado&entry.2055329056=Decreto+1278+de+2002&entry.2057563961=Cmarca&entry.1229277571=Idei&entry.1444558031=1&entry.1026490934=2&entry.269129317=1&entry.1298042726=0&entry.134131095=1&entry.965515678=1&entry.1156841145=0&entry.1364704044=1&entry.1109956124=1&entry.643628948=2&entry.1188526696=2&entry.945005970=1&entry.1205006325=2&entry.975896223=2&entry.1809757995=2&entry.15588726=2&entry.1895296045=2&entry.1180829362=1&entry.1722097493=1&entry.1409288934=2&entry.1932445958=2&entry.2730175=1&entry.1772354376=1&entry.1020106168=2&entry.1323187994=2&entry.750359508=1&entry.1756700184=1&entry.886186224=2&entry.2104735137=1&entry.326826978=2&entry.897728948=1&entry.221005505=2&entry.2014094651=1&entry.57310449=2&entry.132293132=1&entry.1674161771=2&entry.100476736=1&entry.68444739=2&entry.879669428=2&entry.1348118598=1&entry.1893795485=1&entry.1914131614=2&entry.1658491068=2&entry.1362498238=1&entry.291784763=1&entry.389150359=2&entry.1678963136=2&entry.709887415=1&entry.296108911=2&entry.23575497=1&entry.1640455716=2&entry.543150211=2&entry.991620359=2&entry.1011297224=1&entry.97989396=2&entry.2132279693=2&entry.1152401402=2&entry.1333687825=2&entry.2025947001=1&entry.1760324324=2&entry.1457147085=2&entry.1977251599=2&entry.864560536=1&entry.1454866104=2&entry.1831711108=2&entry.779870544=2&entry.1980468116=1&entry.405603422=2&entry.759214817=2&entry.475436336=2&entry.1577749116=1&entry.867166008=2&entry.300015365=2&entry.1412961250=2&entry.1890098527=1&entry.409643532=2&entry.1053947480=2&entry.1048383661=2&entry.1459922221=1&entry.1811410652=1&entry.63849312=2&entry.53343516=2&entry.1968572966=1&entry.757653363=2&entry.852040138=2&entry.464214423=1&entry.95799373=2&entry.2095612640=2&entry.1157334871=1&entry.1288255243=1
+
+//nuevo prellenado
+//https://docs.google.com/forms/d/e/1FAIpQLSeBySBgMcTKjsB1wW_1n-diGiZ0nNvegPuz9XVtbcq7g6JrTg/viewform?usp=pp_url&entry.26647293=YES&entry.1902537249=12345678&entry.1033726661=ydaza@gitei.edu.co&entry.618703803=Hombre&entry.781656536=21+-+30&entry.805018916=Filosof%C3%ADa&entry.493788756=qwerty&entry.1886189987=Preescolar&entry.1472946729=Aprobado&entry.2055329056=Decreto+1278+de+2002&entry.1156732022=S%C3%AD&entry.467908561=S%C3%AD&entry.2057563961=ARAUCA&entry.700026055=dfghj&entry.1229277571=rtyu&entry.155055639=cvbn&entry.1444558031=1&entry.1026490934=2&entry.269129317=1&entry.1298042726=2&entry.134131095=2&entry.965515678=2&entry.1156841145=1&entry.1364704044=2&entry.1109956124=1&entry.643628948=2&entry.1188526696=2&entry.945005970=1&entry.1205006325=2&entry.975896223=2&entry.1809757995=2&entry.15588726=1&entry.1895296045=1&entry.1180829362=2&entry.1722097493=2&entry.1409288934=0&entry.1932445958=1&entry.2730175=1&entry.1772354376=2&entry.1020106168=2&entry.1323187994=1&entry.750359508=1&entry.1756700184=1&entry.886186224=2&entry.2104735137=2&entry.326826978=1&entry.897728948=1&entry.221005505=1&entry.2014094651=2&entry.57310449=1&entry.132293132=2&entry.1674161771=2&entry.100476736=2&entry.68444739=1&entry.879669428=2&entry.1348118598=2&entry.1893795485=2&entry.1914131614=1&entry.1658491068=1&entry.1362498238=1&entry.291784763=2&entry.389150359=2&entry.1678963136=2&entry.709887415=1&entry.296108911=2&entry.23575497=2&entry.1640455716=1&entry.543150211=1&entry.991620359=2&entry.1011297224=2&entry.97989396=1&entry.2132279693=2&entry.1152401402=2&entry.1333687825=1&entry.2025947001=2&entry.1760324324=2&entry.1457147085=2&entry.1977251599=1&entry.864560536=1&entry.1454866104=2&entry.1831711108=2&entry.779870544=2&entry.1980468116=1&entry.405603422=1&entry.759214817=2&entry.475436336=2&entry.1577749116=1&entry.867166008=1&entry.300015365=2&entry.1412961250=2&entry.1890098527=1&entry.409643532=1&entry.1053947480=1&entry.1048383661=2&entry.1459922221=2&entry.1811410652=1&entry.63849312=2&entry.53343516=2&entry.1968572966=2&entry.757653363=2&entry.852040138=1&entry.464214423=1&entry.95799373=1&entry.2095612640=1&entry.1157334871=1&entry.1288255243=2
+
+//el propio prellenado
+//https://docs.google.com/forms/d/e/1FAIpQLSdXQ_EpRIqUYN2OoOYnR7niWawiPotX4zJUjs71w7XofTqvPg/viewform?usp=pp_url&entry.26647293=Yes+Daz&entry.1902537249=3456789&entry.1726504203=ydaza@gitei.edu.co&entry.618703803=Hombre&entry.781656536=21+-+30&entry.805018916=Educaci%C3%B3n+artistica&entry.493788756=dfgh&entry.1886189987=B%C3%A1sica+primaria&entry.436556009=Aprobado&entry.2055329056=Decreto+1278+de+2002&entry.1156732022=No&entry.1301279214=No&entry.1949248842=kghuh&entry.467908561=No&entry.2057563961=ANTIOQUIA&entry.700026055=ertyui&entry.1229277571=sdfghj&entry.155055639=ertyujmn&entry.304568198=Rural&entry.612676138=Regi%C3%B3n+2+-+Centro&entry.156462154=lkjhgfdrtyuj&entry.1444558031=0&entry.1026490934=1&entry.269129317=1&entry.1298042726=1&entry.134131095=2&entry.965515678=2&entry.1156841145=2&entry.1364704044=1&entry.1109956124=1&entry.643628948=1&entry.1188526696=2&entry.945005970=1&entry.1205006325=1&entry.975896223=2&entry.1809757995=2&entry.15588726=1&entry.1895296045=1&entry.1180829362=0&entry.1722097493=2&entry.1409288934=0&entry.1932445958=1&entry.2730175=1&entry.1772354376=2&entry.1020106168=2&entry.1323187994=1&entry.750359508=2&entry.1756700184=2&entry.886186224=1&entry.2104735137=1&entry.326826978=2&entry.897728948=2&entry.221005505=2&entry.2014094651=1&entry.57310449=2&entry.132293132=1&entry.1674161771=2&entry.100476736=1&entry.68444739=1&entry.879669428=2&entry.1348118598=1&entry.1893795485=2&entry.1914131614=2&entry.1658491068=2&entry.1362498238=2&entry.291784763=1&entry.389150359=1&entry.1678963136=1&entry.709887415=2&entry.296108911=2&entry.23575497=1&entry.1640455716=1&entry.543150211=1&entry.991620359=2&entry.1011297224=2&entry.97989396=1&entry.2132279693=1&entry.1152401402=2&entry.1333687825=2&entry.2025947001=1&entry.1760324324=1&entry.1457147085=2&entry.1977251599=2&entry.864560536=1&entry.1454866104=1&entry.1831711108=2&entry.779870544=2&entry.1980468116=1&entry.405603422=1&entry.759214817=1&entry.475436336=2&entry.1577749116=1&entry.867166008=2&entry.300015365=2&entry.1412961250=1&entry.1890098527=1&entry.409643532=2&entry.1053947480=2&entry.1048383661=1&entry.1459922221=1&entry.1811410652=2&entry.63849312=1&entry.53343516=1&entry.1968572966=1&entry.757653363=2&entry.852040138=2&entry.464214423=2&entry.95799373=1&entry.2095612640=1&entry.1157334871=2&entry.1288255243=1
+
+
+function onFormSubmit(e) {
+  var sheet = SpreadsheetApp.getActiveSheet();
+  var email = e.namedValues['Correo Electrónico'][0];
+  var name = e.namedValues['Nombres y Apellidos'][0];
+  var message='funcioneeeee '
+
+  var categoriesIndices = {
+        "Tecnologica": [11, 17, 2, 22, 23, 27, 28, 29, 32, 34, 35, 44, 52, 60, 83, 87, 89, 9],
+        "Pedagógica": [5, 10, 88, 82, 85, 72, 71, 62, 55, 51, 47, 38, 33, 26, 18, 15, 31, 12],
+        "Comunicativa": [1, 14, 36, 49, 57, 65, 7, 86, 42, 73, 24, 77, 20, 39, 30, 4, 69, 81],
+        "Gestión": [13, 8, 41, 67, 75, 79, 48, 90, 25, 46, 54, 58, 64, 78, 19, 3, 37, 61],
+        "Investigativa": [59, 80, 6, 84, 21, 40, 45, 53, 63, 76, 68, 74, 16, 70, 56, 66, 43, 50],
+    }
+
+   var messages={
+        "Tecnologica": ["Reconoce un amplio espectro de herramientas tecnológicas y algunas formas de integrarlas a la práctica educativa.", "Aplica el conocimiento de una amplia variedad de tecnologías en el diseño de ambientes de aprendizaje innovadores y para plantear soluciones a problemas identificados en el contexto.", "Utiliza diversas herramientas tecnológicas en los procesos educativos, de acuerdo a su rol, área de formación, nivel y contexto en el que se desempeña."],
+        "Pedagógica": ["Identifica nuevas estrategias y metodologías mediadas por las TIC, como herramienta para desempeño profesional.", "Lidera experiencias significativas que involucran ambientes de aprendizaje diferenciados de acuerdo a las necesidades e intereses propias y de los estudiantes.", "Propone proyectos y estrategias de aprendizaje con el uso de TIC para potenciar el aprendizaje de los estudiantes." ],
+        "Comunicativa": ["Emplea diversos canales y lenguajes propios de las TIC para comunicarse con la comunidad educativa.", "Participa en comunidades y publica sus producciones textuales en diversos espacios virtuales y a través de múltiples medios digitales, usando los lenguajes que posibilitan las TIC.", "Desarrolla estrategias de trabajo colaborativo en el contexto escolar a partir de su participación en redes y comunidades con el uso de las TIC."],
+        "Gestión": ["Organiza actividades propias de su quehacer profesional con el uso de las TIC.", "Propone y lidera acciones para optimizar procesos integrados de la gestión escolar.", "Integra las TIC en procesos de dinamización de las gestiones directiva, académica, administrativa y comunitaria de su institución." ],
+        "Investigativa": ["Usa las TIC para hacer registro y seguimiento de lo que vive y observa en su práctica, su contexto y el de sus estudiantes.", "Construye estrategias educativas innovadoras que incluyen la generación colectiva de conocimientos.", "Lidera proyectos de investigación propia y con sus estudiantes."],
+    }
+
+  var answers = [];
+
+  for (var key in e.namedValues) {
+    var index = key.match(/\d+/g);
+     if (index) {
+            answers[index] = {
+                "question": key,
+                "value": e.namedValues[key][0]
+            };
+        }
+  }
+
+
+  var consolidated = {"name":name.replace(/\s+/g, "%20")}
+
+  for (var key in categoriesIndices) {
+    consolidated[key.toLowerCase().substring(0,3)] = categoriesIndices[key].map(function(key) {
+      return parseInt(answers[key]["value"])
+    }).reduce(function(total, value) {
+    return total + value;});
+  };
+
+  var params = Object.keys(consolidated).reduce(function(q,key){
+    return q + key + "=" + consolidated[key] + "&";
+  },"?")
+
+
+  //yes... you can concatenate all in the same lines, but it's more readable
+  var tableStyle = "border=\"1\" style=\"border-collapse: collapse; border: 1px solid lightgray;\"";
+  var thStyle = "style=\"padding: 15px; font-size: 14px; background: gray; color: white;\"";
+  var tdStyle = "style=\"padding: 15px;\"";
+  var table="<table "+ tableStyle + "><tr><th "+ thStyle + ">Competencia</th><th "+ thStyle + ">Nivel de competencia</th><th "+ thStyle + ">Descriptor de la competencia</th><th "+ thStyle + ">Escala de valoración</th></tr>";
+
+  for (var key in categoriesIndices) {
+    var temp=key.toLowerCase().substring(0,3);
+    table=table+"<tr>";
+    table=table+"<td "+ tdStyle + ">"+key+"</td>";
+    if(parseInt(consolidated[temp])<=18){
+      table=table+"<td "+ tdStyle + ">Explorador</td><td "+ tdStyle + ">"+messages[key][0]+"</td>";
+    }else {
+      if(parseInt(consolidated[temp])>30){
+        table=table+"<td "+ tdStyle + ">Innovador</td><td "+ tdStyle + ">"+messages[key][1]+"</td>";
+      } else{
+        table=table+"<td "+ tdStyle + ">Integrador</td><td "+ tdStyle + ">"+messages[key][2]+"</td>";
+      }
+    }
+    table=table+"<td "+ tdStyle + ">"+consolidated[temp]+"</td></tr>";
+  }
+  table=table+"</table>";
+
+
+  var url = "http://www.gitei.edu.co/competencias-tic/" + params;
+  var giteiUrl = "http://www.gitei.edu.co/";
+  var unalUrl = "http://unal.edu.co/";
+  var giteiLogoUrl = giteiUrl + "competencias-tic/encuesta/img/logo_gitei.png";
+  var unalLogoUrl = giteiUrl + "competencias-tic/encuesta/img/logo_unal.png";
+  var separatorLogoUrl = giteiUrl + "competencias-tic/encuesta/img/logo_separator.png";
+  var mailBody;
+  var subjectBody = "Realimentación Auto-Reporte: Instrumento de Competencias TIC para el desarrollo profesional docente";
+  var introductoryText = 'Apreciado docente/directivo docente ' + name + ': Ha recibido el resultado del Auto reporte: Instrumento de Competencias TIC para el desarrollo profesional docente. A continuación encontrará su resultado y algunas recomendaciones basadas en las competencias TIC y el nivel de desempeño. Recuerde, la formación en estas competencias es fundamental para su desarrollo profesional.';
+  var ratingScale = '<b>*Escala de valoración</b><br>0-18: Nivel Explorador<br>19-30: Nivel Integrador<br>31-36: Nivel Innovador';
+  var feedbackInfo = '<p>Las siguientes son recomendaciones generales que le permitirán guiar su proceso de desarrollo profesional para innovar su práctica educativa con uso de TIC, así como la invitación para adoptar estrategias que le permitan fortalecer dichas competencias desde su contexto escolar.</p>';
+  var explorerInfo = '<p><b>Nivel Explorador:</b> Estar en este nivel significa que ha decidido indagar en el uso de tecnologías en sus procesos de aula y es sin duda el principal punto de partida. Le recomendamos lo siguiente:</p><ol><li>Aproveche las TIC para aprender por iniciativa personal y/o actualizarse en su área de conocimiento</li><li>Incluya en sus actividades de aprendizaje herramientas TIC, medios audiovisuales, contenidos digitales, como por ejemplo, aquellos que se encuentran disponibles en el Portal Colombia Aprende</li><li>Indague diferentes metodologías innovadoras en el aula que incluyan el uso de las TIC e implemente la que mejor se adapte a su contexto escolar.</li></ol>';
+  var integratorInfo = '<p><b>Nivel Integrador:</b> Estar en este nivel significa que ha llevado a cabo procesos educativos en los que hace uso de las TIC y genera con ellas actividades con objetivos específicos. Le recomendamos lo siguiente: </p><ol><li>Implemente en sus actividades de aula, metodologías innovadoras que promuevan entre los estudiantes el aprendizaje autónomo y colaborativo con uso de TIC.</li><li>Diseñe y documente una actividad de aprendizaje que incluya contenidos digitales u objetos virtuales de aprendizaje haciendo uso de las TIC.</li><li>Publique los resultados de la actividad en portales académicos, blogs, comunidades de práctica, y esté abierto a la retroalimentación que reciba de ello.</li></ol>';
+  var innovatorInfo = '<p><b>Nivel innovador:</b>Lograr el nivel innovador significa que ha usado las TIC para crear, expresar sus ideas y construir colectivamente nuevos conocimientos, incluyendo estrategias novedosas para reconfigurar su práctica educativa. Le recomendamos lo siguiente: </p><ol><li>Proponga proyectos educativos mediados con TIC, que promuevan la producción de nuevo conocimiento.</li><li>Diseñe ambientes de aprendizaje mediados por las TIC, teniendo en cuenta las características propias de sus estudiantes, para fortalecer el desarrollo de las diferentes competencias.</li><li>Documente los resultados obtenidos, construyendo material que le permita participar en diferentes escenarios nacionales e internacionales compartiendo su experiencia pedagógica con uso de TIC.</li></ol>Si desea profundizar más, siga el enlace <a href="https://www.colombiaaprende.edu.co/html/micrositios/1752/articles-318264_recurso_tic.pdf">COMPETENCIAS TIC PARA EL DESARROLLO PROFESIONAL DOCENTE</a>';
+
+  var graphUrl='https://chart.googleapis.com/chart?cht=r&chs=500x500&chd=t:72,72,72,72,72,72%7C60,60,60,60,60,60%7C36,36,36,36,36,36%7C'+(consolidated['tec']*2)+','+(consolidated['ped']*2)+','+(consolidated['com']*2)+','+(consolidated['ges']*2)+','+(consolidated['inv']*2)+','+(consolidated['tec']*2)+'&chco=97CBEF30,6F9ED380,0036FFA0,FF9900&chls=1.0,1.0,0.0%7C1.0,1.0,0.0%7C1.0,1.0,0.0%7C2.0,4.0,0.0&chxt=x&chxl=0:%7CTecnol%C3%B3gica%7CPedag%C3%B3gica%7CComunicativa%7CGesti%C3%B3n%7CInvestigativa&chm=B,97CBEF30,0,1.0,5.0%7CB,6F9ED380,1,1.0,5.0%7CB,0036FFA0,2,1.0,5.0%7CB,FF9900C0,3,1.0,5.0&chdlp=t&chdl=Innovador|Integrador|Explorador|'+name;
+  Logger.log('La url esta bien!');
+  //var graphBlob = UrlFetchApp.fetch(graphUrl).getBlob().setName("graphBlob");
+
+  //var htmlEmail='<p>pruebaaaaaaa</p><iframe src=\"'+url+'\"></iframe> <p>fiiiiinnnn</p>'
+
+  //https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf
+  //MailApp.sendEmail(email, "fuuuu", message,{htmlbody:htmlEmail});
+
+
+
+  mailBody = "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\">";
+  mailBody += introductoryText + '<br><br>';
+  mailBody += table + '<br><br>';
+  mailBody += ratingScale + '<br><br>';
+  mailBody += '<img src='+ graphUrl +'>';
+  mailBody += feedbackInfo;
+  mailBody += explorerInfo;
+  mailBody += integratorInfo;
+  mailBody += innovatorInfo + '<br><br>';
+  mailBody += 'También están disponibles en el enlace <a href=' + url + '>RESULTADOS DE LA PRUEBA</a><br><br>';
+  mailBody += '<a href="'+ giteiUrl + '"><img style="width: 49%; max-width: 264px;" src=' + giteiLogoUrl + '></a>';
+  mailBody += '<img style="width: 0.555%; max-width: 3px;" src=' + separatorLogoUrl + '>';
+  mailBody += '<a href="'+ unalUrl + '"><img style="width: 38%; max-width: 204px;" src=' + unalLogoUrl + '></a>';
+  MailApp.sendEmail(email, subjectBody , message,{htmlBody: mailBody});
+
+}
+
+ function loadJSFromHTMLFile() {
+  eval(UrlFetchApp.fetch('https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.min.js').getContentText());
+ }
+
+ function loadJSFromHTMLFile() {
+  eval(UrlFetchApp.fetch('https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.min.js').getContentText());
+ }
